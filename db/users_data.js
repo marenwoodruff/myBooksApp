428157e01e4ad364a3c19ca284d3f92e85200c40
MarenWoodruff <wren403@gmail.com>
2018-04-24 18:19:54 -0400

    added user seeds

---
+++ db/users_data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  [
+    {
+      first_name: "Schmitty",
+      last_name: "Schmitt",
+      email: "schmitty@gmail.com",
+      passwored: "123456"
+    },
+    {
+      first_name: "Mike",
+      last_name: "Dang",
+      email: "mikeyD@gmail.com",
+      passwored: "123456"
+    }
+  ]
+};
