9c816ec6b85231cc8175023854c7741655e396d9
MarenWoodruff <wren403@gmail.com>
2018-04-24 18:22:31 -0400

    added book seeds

---
+++ db/books_data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  [
+    {
+      title: "The Eyre Affair: A Thursday Next Novel",
+      genre: "fiction",
+      description: "A literary detective pursues a master criminal through the world of Charlotte Bronte's Jane Eyre",
+      user_id: 1
+    },
+    {
+      title: "Bad Feminist: Essays",
+      genre: "non-fiction",
+      description: "Bad Feminist explores being a feminist while loving things that could seem at odds with feminist ideology",
+      user_id: 2
+    }
+  ]
+};
