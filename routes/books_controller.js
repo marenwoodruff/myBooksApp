f7e6d2705d5dc7ce9a98d33ae8e539289e430f91
MarenWoodruff <wren403@gmail.com>
2018-04-24 23:22:25 -0400

    added edit view + edit route + update route

--- routes/books_controller.js
+++ routes/books_controller.js
@@ -20,8 +20,26 @@ router.get('/', (req, res) => {
 // new route
 router.get('/new', (req, res) => {
   // res.send('we knew it');
-  
-  res.render('books/new');
+
+  res.render('books/new', {
+    // building an instance of book
+    book: Book.build()
+  })
+  .catch((err) => {
+    res.status(400).render('error');
+  });
+});
+
+// create route
+router.post('/', (req, res) => {
+  // requires an object with properties that map to the properties of the object
+  Book.create(req.body)
+    .then((hobby) => {
+      res.redirect('/books');
+    })
+    .catch((err) => {
+      res.status(400).render('error');
+    });
 });
 
 // show route
@@ -32,6 +50,37 @@ router.get('/:id', (req, res) => {
     .then((book) => {
       res.render('books/show', {
          book
+      });
+    })
+    .catch((err) => {
+      res.status(400).render('error');
+    });
+});
+
+// edit route
+router.get('/:id/edit', (req, res) => {
+  // res.send('we do it');
+  Book.findById(req.params.id)
+    .then((book) => {
+      res.render('books/edit', {
+      	  id: req.params.id,
+         book
+      });
+    })
+    .catch((err) => {
+      res.status(400).render('error');
+    });
+});
+
+// update route
+router.put('/:id', (req, res) => {
+  Book.findById(req.params.id)
+    .then((book) => {
+      return book.update(req.body); // update method returns a promise
+    })
+    .then((updatedBook) => { // the book parameter is the updated hobby
+      res.render('books/show', {
+        book: updatedBook
       });
     })
     .catch((err) => {
